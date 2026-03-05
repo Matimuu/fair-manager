4b60e26335423b9cd0e056d0a3c9ad26147f3d63
omarenrique <alekseich18@gmail.com>
2026-03-05 15:30:35 +0100

    fix: in CreateProductRequest change annotation not null to not blank

--- common-data/src/main/java/es/mpoea/fairmanager/commondata/DTO/requests/Product/CreateProductRequest.java
+++ common-data/src/main/java/es/mpoea/fairmanager/commondata/DTO/requests/Product/CreateProductRequest.java
@@ -1,17 +1,17 @@
 package es.mpoea.fairmanager.commondata.DTO.requests.Product;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateProductRequest(
 
-        @NotNull
+        @NotBlank
         @Size(max = 240)
         String label,
 
         String description,
 
-        @NotNull
+        @NotBlank
         @Size(max = 120)
         String category
 ) {
