201904b6fd890a2c00c75a575a07fe7cdbd1fc16
omarenrique <alekseich18@gmail.com>
2026-03-05 15:28:21 +0100

    feat: add blank endpoints in controller

--- Product-service-api/src/main/java/es/mpoea/fairmanager/product_service/api/controllers/ProductController.java
+++ Product-service-api/src/main/java/es/mpoea/fairmanager/product_service/api/controllers/ProductController.java
@@ -2,8 +2,9 @@ package es.mpoea.fairmanager.product_service.api.controllers;
 
 import es.mpoea.fairmanager.product_service.api.services.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+//TODO: Implement all endpoints and return appropriate response.
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -12,5 +13,29 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @PostMapping()
+    public void createProduct() {
+
+    }
+
+    @PutMapping()
+    public void updateProduct() {
+
+    }
+
+    @DeleteMapping()
+    public void deleteProduct() {
+
+    }
+
+    @GetMapping()
+    public void getAllProducts() {
+
+    }
+
+    @GetMapping("/{id}")
+    public void getProductById(@PathVariable long id) {
+
+    }
 
 }
